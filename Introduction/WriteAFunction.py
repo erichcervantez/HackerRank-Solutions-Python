3c75cd08bbd6a733ff0e139cfd8b96625a418028
Erich Cervantez <git@noofusion.com>
2016-09-07 18:34:56 -0700

    Removing PEP 8 warnings

--- Introduction/WriteAFunction.py
+++ Introduction/WriteAFunction.py
@@ -1,20 +1,24 @@
 # https://www.hackerrank.com/challenges/write-a-function
 
+
 def is_leap(year):
     leap = False
 
-    if (year % 4 == 0):
+    if year % 4 == 0:
         leap = True
 
-    if (year % 100 == 0):
+    if year % 100 == 0:
         leap = False
 
-        if (year % 400 == 0):
+        if year % 400 == 0:
             leap = True
 
     return leap
 
 
-year = int(input())
+def main():
+    year = int(input())
+    print(is_leap(year))
 
-print(is_leap(year))
+
+main()
